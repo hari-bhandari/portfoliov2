fcdaf667f68a19e6e25cf3d1c5f8d2f0fb17ec89
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-11-15 20:16:10 +0000

    seperated styled css from loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,40 +3,8 @@ import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import anime from 'animejs';
 import IconLoader from "./loader";
-import styled from 'styled-components';
 import theme from "../../styles/theme";
-import mixins from "../../styles/mixins";
-const { colors } = theme;
-
-const StyledContainer = styled.div`
-  ${mixins.flexCenter};
-  background-color: ${colors.darkNavy};
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  bottom: 0;
-  left: 0;\
-  right: 0;
-  z-index: 99;
-`;
-const StyledLogo = styled.div`
-  width: max-content;
-  max-width: 400px;
-  transition: ${theme.transition};
-  opacity: ${props => (props.isMounted ? 1 : 0)};
-  svg {
-    width: 100%;
-    height: 100%;
-    display: block;
-    margin: 0 auto;
-    fill: none;
-    user-select: none;
-    #B {
-      opacity: 0;
-    }
-  }
-`;
+import {StyledContainer,StyledLogo} from "./LoaderCSS";
 
 const Loader = ({ finishLoading }) => {
     const animate = () => {
@@ -86,7 +54,6 @@ const Loader = ({ finishLoading }) => {
     return (
         <StyledContainer className="loader">
             <Helmet bodyAttributes={{ class: `hidden` }} />
-
             <StyledLogo isMounted={isMounted}>
                 <IconLoader />
             </StyledLogo>
---
+++ src/components/Loader/LoaderCSS.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import mixins from "../../styles/mixins";
+import theme from "../../styles/theme";
+
+export const StyledContainer = styled.div`
+  ${mixins.flexCenter};
+  background-color: ${colors.darkNavy};
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;\
+  right: 0;
+  z-index: 99;
+`;
+export const StyledLogo = styled.div`
+  width: max-content;
+  max-width: 400px;
+  transition: ${theme.transition};
+  opacity: ${props => (props.isMounted ? 1 : 0)};
+  svg {
+    width: 100%;
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+    fill: none;
+    user-select: none;
+    #B {
+      opacity: 0;
+    }
+  }
+`;
