837ab4d5247e881c16fd70be49d475465b759e76
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-11-21 10:08:50 +0000

    merged code wrapper and color wrapper

--- src/components/Cards/CodingCard.js
+++ src/components/Cards/CodingCard.js
@@ -1,75 +1,42 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { withTheme } from 'styled-components';
-import { FaPython, FaJs,FaJava } from 'react-icons/fa';
-import {
-    HeroCardWrapper,
-    CodeCardWrapper,
-    ColorPaletteWrapper,
-    ColorBoxWrapper,
-} from './CodingCardCSS';
-
-function repeatString(str, count) {
-    let maxCount = str.length * count;
-    count = Math.floor(Math.log(count) / Math.log(2));
-    while (count) {
-        str += str;
-        count--;
-    }
-    str += str.substring(0, maxCount - str.length);
-    return str;
-}
+import React, {useState} from 'react';
+import {FaJava, FaJs, FaPython} from 'react-icons/fa';
+import {codeObject} from "./CodingList";
+import {CodeCardWrapper, ColorPaletteWrapper, HeroCardWrapper,} from './CodingCardCSS';
 
 
 export const CodeCard = () => {
-    const [text, setText] = useState(`new Date().getFullYear() - 2001;`);
-    const age = new Date().getFullYear() - 2001;
-    // trimed down polyfill of String.repeat
-    const changeText = () => {
-        let space = repeatString(' ', 54);
-        setText(age + ';' + space);
-    };
+    const [language, setLanguage] = useState('js')
+    const [text, setText] = useState(codeObject['js'])
+    const onClickForPL = (language, text) => {
+        setLanguage(language)
+        setText(text)
+    }
+
     return (
-        <CodeCardWrapper>
+        <>
+            <CodeCardWrapper>
       <pre>
         1&nbsp;&nbsp;class <b>Person</b> {'{'}
       </pre>
-            <pre onClick={changeText}>5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.age = {text}</pre>
-            <pre>7&nbsp;&nbsp;{'}'}</pre>
-        </CodeCardWrapper>
+                <pre>2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{text[1]}</pre>
+                <pre>2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{text[2]}</pre>
+                <pre>2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{text[3]}</pre>
+                <pre>2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{'}'}</pre>
+                <pre>3&nbsp;&nbsp;{'}'}</pre>
+            </CodeCardWrapper>
+            <ColorPaletteWrapper>
+                <FaJs size={10} onClick={onClickForPL('python',codeObject['python'])}/>
+                <FaJava/>
+                <FaPython/>
+            </ColorPaletteWrapper>
+        </>
     );
 };
-
-const ColorBox = ({ color }) => {
-    const tooltipRef = useRef();
-    useEffect(() => {
-        return tooltipRef.current.addEventListener('animationend', () => {
-            tooltipRef.current.classList.remove('tooltip-animate');
-        });
-    });
-
-    return (
-        <ColorBoxWrapper
-            ref={tooltipRef}
-            style={{ background: color }}
-        />
-    );
-};
-
-export const ColorPalette = withTheme(({ theme }) => {
-    return (
-        <ColorPaletteWrapper>
-            <FaJs size={10}/>
-            <FaJava/>
-            <FaPython/>
-        </ColorPaletteWrapper>
-    );
-});
 
 export const HeroCard = () => {
     return (
         <HeroCardWrapper>
             <CodeCard />
-            <ColorPalette />
         </HeroCardWrapper>
     );
 };
