8572e802fcb5e020b9d3ead6d20ce29d2af486ac
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-12-07 16:26:48 +0000

    implemented graphql query on about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,7 +4,20 @@ import sr from '../../utils/sr'
 import {srConfig} from '../../config'
 import Image from "../image";
 import PageHeader from "../../commons/PageHeader";
+import {graphql,useStaticQuery} from "gatsby";
+
 const AboutMe = () => {
+    const data = useStaticQuery(graphql`
+    {
+      aboutJson {
+        about
+        aboutSecond
+        intro
+        skills
+      }
+    }
+  `)
+    const about=data.aboutJson
 
     const revealContainer = useRef(null);
 
@@ -12,36 +25,24 @@ const AboutMe = () => {
         sr.reveal(revealContainer.current, srConfig());
     }, []);
 
-    const skills = ['JavaScript (ES6+)', 'HTML & (S)CSS', 'React', 'BootStrap','Python','Java', 'Node.js', 'MongoDB'];
-
     return (
         <StyledAboutSection id="about" ref={revealContainer}>
             <PageHeader>About Me</PageHeader>
             <div className="inner">
                 <StyledText>
                     <div>
-                        <p>Hi,I'm Hari Bhandari,</p>
+                        <p>{about.intro}</p>
 
-                        <p>
-                            A passionate self-taught developer. I like to describe myself as someone persistent and purposeful;
-                            I love solving real-life problems, mathematics problems and learning new algorithms.
-                        </p>
+                        <p>{about.about}</p>
 
-                        <p>
-                            I started coding back in 2019 after I graduated from my sixth form with AAB. I was impassive
-                            about algorithms, but Advanced level further decision wholly altered my mind.
-                            It taught me different aspects of programming from learning algorithms to being more Logical.
-                            Also, Advanced level Mathematics and Further Mathematics has enhanced my mathematical skills,
-                            problems solving skills and most importantly, my analytical skills. I want to pursue a career
-                            in computer programming!
-                            Furthermore, I am also interested in creating and learning optimised algorithms.
+                        <p>{about.aboutSecond}
                         </p>
 
                         <p>Here are a few technologies I've been working with recently:</p>
                     </div>
 
                     <ul className="skills-list">
-                        {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+                        {about.skills && about.skills.map((skill, i) => <li key={i}>{skill}</li>)}
                     </ul>
                 </StyledText>
 
