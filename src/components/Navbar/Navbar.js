6d2eec9c77e989ac16bfb09947a8f0c980a1d54d
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-12-08 09:37:27 +0000

    added resume

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -32,8 +32,8 @@ const Navigation = () => {
             <MobileNavContainer className={`${openNav ? 'open' : 'closed'}`}>
                 <MobileNav className={`${openNav ? 'open' : 'closed'}`}>
                     <NavLinks>
-                        <Link href="#projects" className="nav-link" style={{ transitionDelay: `${openNav ? '100ms' : '250ms'}` }} offset={-30} to="projects" spy={true} smooth={true} delay={400} duration={500} >Projects</Link>
-                        <Link href="#experience" className="nav-link" style={{ transitionDelay: `${openNav ? '150ms' : '200ms'}` }} offset={-30} to="experience" spy={true} smooth={true} delay={400} duration={500} >Experience</Link>
+                        <Link href="#about" className="nav-link" style={{ transitionDelay: `${openNav ? '100ms' : '250ms'}` }} offset={-30} to="about" spy={true} smooth={true} delay={400} duration={500} >About</Link>
+                        <Link href="#projects" className="nav-link" style={{ transitionDelay: `${openNav ? '150ms' : '200ms'}` }} offset={-30} to="experience" spy={true} smooth={true} delay={400} duration={500} >Projects</Link>
                         <Link href="#contact" className="nav-link" style={{ transitionDelay: `${openNav ? '200ms' : '150ms'}` }} offset={-30} to="contact" spy={true} smooth={true} delay={400} duration={500} >Contact</Link>
                         <a className="nav-link" href="/resume.pdf" target="_blank" rel="noopener noreferrer" style={{ transitionDelay: `${openNav ? '250ms' : '100ms'}` }} >Resume</a>
                     </NavLinks>
