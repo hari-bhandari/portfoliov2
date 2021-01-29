4a03841010b04087eba4a798612fde38fd5f23d1
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-29 20:01:59 +0000

    renamed clones.json to projectClones.json

---
+++ src/components/projects/Clones.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+import { CreativeCodingWrapper } from './MiniProjectsCSS';
+import MiniProject from './MiniProject';
+
+import Grid from "../../commons/Grid";
+import PageHeader from "../../commons/PageHeader";
+import Button from "../../commons/Button";
+
+const Clones = () => {
+    const [showAll, setShowAll] = useState(false);
+
+    const miniProjects = useStaticQuery(graphql`
+    {
+      allUnFeaturedProjectsJson {
+        edges {
+          node {
+            id
+            demo
+            picture
+            title
+            github
+          }
+        }
+      }
+    }
+  `)
+
+    const handleShowAll = () => {
+        setShowAll(true);
+    };
+
+    return (
+        <CreativeCodingWrapper >
+            <PageHeader>More Coding Projects</PageHeader>
+
+            <Grid collapseHeight="700px" showAll={showAll}>
+                {miniProjects.allUnFeaturedProjectsJson.edges.map(({ node }) => (
+                    <MiniProject key={node.id} node={node} />
+                ))}
+                {!showAll && (
+                    <Button onClick={handleShowAll} className="showall__button">
+                        Show all
+                    </Button>
+                )}
+            </Grid>
+        </CreativeCodingWrapper>
+    );
+};
+
+export default Clones;
