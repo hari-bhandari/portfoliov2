2f0ad27913b62d1c5c47533068c0a3ebd0715389
hari-bhandari <2012bhandari.ha@gmail.com>
2021-10-05 17:49:07 +0100

    few

--- src/styles/index.js
+++
@@ -1,7 +0,0 @@
-import theme from "./theme"
-import Container from "./container"
-import Section from "./Section"
-import media from "./media"
-import GlobalStyles from "./GlobalStyle"
-
-export { theme, Container, Section, media, GlobalStyles }
