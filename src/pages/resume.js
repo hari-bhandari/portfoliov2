79aba8ec3a2926bc3384e03751431bb3e609b9f6
hari-bhandari <2012bhandari.ha@gmail.com>
2021-10-05 18:14:08 +0100

    added resume pdf

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -8,7 +8,7 @@ export default function Test() {
   return (
     <PdfContainer data="resume.pdf">
       <p>It appears you don't have a PDF plugin for this browser.
-        No biggie... you can <a href="../../static/resume.pdf">click here to
+        No biggie... you can <a href="/resume.pdf">click here to
           download the PDF file.</a></p>
     </PdfContainer>
   );
