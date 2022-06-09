d8dd13579be2cdcd73eb00d757218f2b93952674
Hari Bhandari <2012bhandari.ha@gmail.com>
2022-06-09 17:16:26 +0100

    added seo on resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import Layout from "../components/layout";
-const PdfContainer=styled.object`
+import SEO from "../components/seo";
+
+const PdfContainer = styled.object`
   width: 100%;
   height: 100vh;
-`
+`;
 export default function Test() {
   return (
-    <Layout>
-    <PdfContainer data="resume.pdf">
-      <p>It appears you don't have a PDF plugin for this browser.
-        No biggie... you can <a href="/resume.pdf">click here to
-          download the PDF file.</a></p>
-    </PdfContainer>
-    </Layout>
-  );
+    <>
+      <SEO lang={"en"} />
+      <PdfContainer data="resume.pdf">
+        <p>It appears you don't have a PDF plugin for this browser.
+          No biggie... you can <a href="/resume.pdf">click here to
+            download the PDF file.</a></p>
+      </PdfContainer>;
+    </>
+  )
+    ;
 }
