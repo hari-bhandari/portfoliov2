ccd89833cbb0a5a720ac08e06d603abd15bb743c
Hari Bhandari <61126375+hari-bhandari@users.noreply.github.com>
2022-07-11 13:56:45 +0100

    change linkedin url

--- src/components/Side/socialIconLinks.js
+++ src/components/Side/socialIconLinks.js
@@ -8,7 +8,7 @@ const SocialIconLinks = ({ iconClasses }) => {
       <a href="https://github.com/hari-bhandari" target="_blank" rel="noopener noreferrer">
         <FaGithub height={'31.665px'} width={'31.665px'} className={iconClasses}/>
         </a>
-      <a href="https://www.linkedin.com/in/haribhandari0" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/haribhandari1010" target="_blank" rel="noopener noreferrer">
         <FaLinkedin height={'31.665px'} width={'31.665px'} className={iconClasses}/>
       </a>
       <a href="https://twitter.com/_hari_bhandari" target="_blank" rel="noopener noreferrer">
